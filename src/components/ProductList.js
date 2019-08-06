cb2be84dbecd2ef3b965ec74f95dc74ed2e36e55
J3DM <josemdmarcos@gmail.com>
2019-08-06 11:44:47 +0200

    Fixed no animation in toggle

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -20,13 +20,13 @@ class ProductList extends React.Component{
         var showAllProducts=""
         if(this.props.user.role==="ADMIN" ||this.props.user.role==="MANAGER" ){
             showAllProducts= <div className="col-3"><div className="form in-line custom-control custom-switch">
-                <label className="custom-control-label" htmlFor="customSwitch1">Show deleted products</label>
                 <input 
                     type="checkbox" 
                     className="custom-control-input" 
                     id="customSwitch1" 
-                    checked={this.props.showAll} name="showAll" 
+                    checked={this.props.showAll} name="showAll"
                     onChange={this.props.onChange}/>
+                <label className="custom-control-label" htmlFor="customSwitch1">Show deleted products</label>
             </div></div>
         }
         var productList

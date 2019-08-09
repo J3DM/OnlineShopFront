c18d537a6b9d98f76307327b402c4bf4489baa99
J3DM <josemdmarcos@gmail.com>
2019-08-09 17:20:18 +0200

    Updated build

---
+++ build/precache-manifest.e5c5fe5754b757483a504aa8a91b4b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac079972e7fe5eda134e623ecde81ef0",
+    "url": "/OnlineShopFront/index.html"
+  },
+  {
+    "revision": "2c62024b6116ba5a9d62",
+    "url": "/OnlineShopFront/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "533e0c55c3e296a59416",
+    "url": "/OnlineShopFront/static/js/2.dbf0c8e4.chunk.js"
+  },
+  {
+    "revision": "2c62024b6116ba5a9d62",
+    "url": "/OnlineShopFront/static/js/main.08ce9aaf.chunk.js"
+  },
+  {
+    "revision": "ff3e0012c87558cc66ab",
+    "url": "/OnlineShopFront/static/js/runtime~main.c70b7a77.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71f8e9052d3ab5df53c9a528934ad059.js"
+  "/OnlineShopFront/precache-manifest.e5c5fe5754b757483a504aa8a91b4b8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OnlineShopFront/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

4abd83d9508d92ea758ebecd07624cb66b8c0824
J3DM <josemdmarcos@gmail.com>
2019-08-09 16:57:06 +0200

    Updates

---
+++ precache-manifest.ede0963302c473da8766affbb978eb78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8662234be3f8871014994fe00e2ef62f",
+    "url": "/OnlineShopFront/index.html"
+  },
+  {
+    "revision": "6fb3645a7e70b7e96770",
+    "url": "/OnlineShopFront/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "533e0c55c3e296a59416",
+    "url": "/OnlineShopFront/static/js/2.dbf0c8e4.chunk.js"
+  },
+  {
+    "revision": "6fb3645a7e70b7e96770",
+    "url": "/OnlineShopFront/static/js/main.e116c9a0.chunk.js"
+  },
+  {
+    "revision": "ff3e0012c87558cc66ab",
+    "url": "/OnlineShopFront/static/js/runtime~main.c70b7a77.js"
+  }
+]);
